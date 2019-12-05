c81a4705e5835fda35deaefec480681e1ef94204
0624078 <55527287+0624078@users.noreply.github.com>
2019-12-05 16:18:48 +0800

    Update CT.java

--- CT.java
+++ CT.java
@@ -1,15 +1,30 @@
-abstract class CShape
-{
+abstract class CShape{
+    protected String color;
+    
+    public void setcoler(String str){
+        color=str;  
+      }
+
+      
+  
+   public void show(){
 
 
-
-
+    }
 }
 
-  
-  
-  public void show()
-{
+class CTriangle extends CShape{
+ double llong,width,hypotenuse
+ public void setside(double a,b,c){
+ llong=a;
+ width=b;
+ hypotenuse=c;
+ }
+ 
+ public void show(){
+ System.out.print("面積為:"+llong*width/2)
+ System.out.print("color:"+color)
+ }
 
 
 }
